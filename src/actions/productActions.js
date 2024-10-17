69a2797d75d92b6f7f63535f0887f8bb061b1012
caale18 <crodriguezd10@miumg.edu.gt>
2024-10-17 13:38:01 -0600

    b

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -13,10 +13,10 @@ export const getProducts = (keyword = '', currentPage = 1, price, category) => a
     try {
         dispatch({ type: ALL_PRODUCTS_REQUEST });
 
-        let link =  `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[lte]=${price[1]}&price[gte]=${price[0]}`
+        let link =  `https://produccionbackend.onrender.com/api/v1/products?keyword=${keyword}&page=${currentPage}&price[lte]=${price[1]}&price[gte]=${price[0]}`
 
         if(category) {
-            link =  `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[lte]=${price[1]}&price[gte]=${price[0]}&category=${category}`
+            link =  `https://produccionbackend.onrender.com/api/v1/products?keyword=${keyword}&page=${currentPage}&price[lte]=${price[1]}&price[gte]=${price[0]}&category=${category}`
         }
 
         const { data } = await axios.get(link);
